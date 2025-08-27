ade1314a89c0058f47adb293a6545e6367ecaf9f
Akshit08gu <akgupta7372@gmail.com>
2025-08-27 19:28:41 +0530

    origin : true

--- backend/index.js
+++ backend/index.js
@@ -33,7 +33,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieparser());
 const corsOptions = {
-  origin:  "http://localhost:5173",
+  origin: true,
   credentials: true,
 }
 app.use(cors(corsOptions));
